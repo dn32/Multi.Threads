2c915f95dac6a59519bcfe3d7a502f8a1d7bd3d5
Marcelo <dn@dn32.com.br>
2017-09-19 11:52:37 -0300

    Melhorias

---
+++ Multi.Threads.Core/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Multi.Threads.Core
+{
+    public class Task
+    {
+        public Task(object parameter)
+        {
+            this.Parameter = parameter;
+        }
+
+        public object Parameter { get; private set; }
+        public Exception Exception { get; internal set; }
+        public object Return { get; internal set; }
+    }
+}
